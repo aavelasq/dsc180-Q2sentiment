5f066b39288f0dd0e3fe5ad394e459e86927b1f1
Nikitha Gopal <49108547+nvgopal@users.noreply.github.com>
2022-01-27 22:46:24 -0800

    updated vader script w/ plots

--- src/vader_script.py
+++ src/vader_script.py
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import os
 import pandas as pd
 import pandas.util.testing as tm
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
 import datetime
 import matplotlib.dates as mdates
 from eda import convert_dates
@@ -31,31 +29,6 @@ def plotPolarity(meanPolar, target):
     plt.savefig(out_path_png,dpi=300, bbox_inches = "tight")
     plt.close()
 
-
-def polarityFunc(data, target):
-    '''
-    calculates mean polarity before and after controversy date
-    outputs line plot and polarity over cancellation period data 
-    using Vader Sentiment library
-    '''
-
-    data = convert_dates(data)
-    data["Days Before & After Controversy"] = (data["created_at"] - cancellation_date).dt.days
-
-    analyser = SentimentIntensityAnalyzer()
-    scores = []
-
-    for tweet in data['text']: 
-        polarity_val = analyser.polarity_scores(tweet)
-        scores.append(polarity_val)
-    print(len(scores))
-
-=======
-import pandas as pd
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-tempdir = ".//data/temp/"
-
 def polarityFunc(data, target):
 
     main_df = data
@@ -73,14 +46,12 @@ def polarityFunc(data, target):
     file_name = tempdir + target + '_vaderPolarity.csv'
     
     print(len(scores))
->>>>>>> 47f12acb9f336f8398c9e00b439721a410314d9f
     temp_df = pd.DataFrame(scores)
 
     data['Compound'] = temp_df['compound']
     data['Negative'] = temp_df['neg']
     data['Neutral'] = temp_df['neu']
     data['Positive'] = temp_df['pos']
-<<<<<<< HEAD
 
     pol_mean_daily = data.groupby("Days Before & After Controversy").mean()['Compound']
     
@@ -89,8 +60,5 @@ def polarityFunc(data, target):
     file_name = tempdir + target + '_vaderPolarity.csv'
     pol_mean_daily = pol_mean_daily.reset_index()
     pol_mean_daily.to_csv(file_name, index=False)
-=======
     
     main_df.to_csv(file_name, index=False)
-
->>>>>>> 47f12acb9f336f8398c9e00b439721a410314d9f
